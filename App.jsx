9015e6ec6d4d6033cb290702e263973b13618e7e
gouthamkd <you@example.com>
2017-11-10 23:20:16 +0530

    sample environment setup to run the react js app

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello Goutham , Welcome to React!!!!!</div>
+    );
+  }
+}
+
+export default App;
